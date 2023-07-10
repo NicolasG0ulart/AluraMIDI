2331aa13f07a7ca099ed6b1e0ea68674968a73b9
Nicolas <nicolasgoulart5001@gmail.com>
2023-07-10 16:21:51 -0300

    finish project

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
-function tocaSom(idElementoAudio){
-    document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio){
+    const elemento = document.querySelector(seletorAudio);
+
+    if(elemento === null){
+        // alert('Elemento não encontrado')
+        console.log('Elemento não encontrado')
+    }else{
+        elemento.play();
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
 
-
-while(contador < listaDeTeclas.length){
+for(let contador = 0; contador < listaDeTeclas.length; contador++){
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
@@ -16,6 +21,15 @@ while(contador < listaDeTeclas.length){
     tecla.onclick = function (){ 
         tocaSom(IdAudio)
     }
-    
-    contador = contador + 1;
+
+    tecla.onkeydown = function (event){
+        
+        if(event.code === 'Space' || event.code === 'Enter'){
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
 }
